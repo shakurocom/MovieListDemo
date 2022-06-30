a46f39e24848188202d27f4326335d87868bdb63
EugeneKlyuenkov <dawd>
2022-06-30 10:56:03 +0300

    MAR-390-[added bundle helper]

---
+++ MovieListControl/Source/Extensions/Bundle+Helper.swift
@@ -0,0 +1,18 @@
+//
+//  Bundle+Helper.swift
+//  MovieList
+//
+
+import Foundation
+
+extension Bundle {
+
+    static func findBundleIfNeeded(for aClass: AnyClass) -> Bundle {
+        if let podBundleURL = Bundle(for: aClass).url(forResource: "MovieList", withExtension: "bundle"),
+           let podBundle = Bundle(url: podBundleURL) {
+            return podBundle
+        }
+        return Bundle.main
+    }
+
+}
